a563bf703ce407756838df250a44f1f86565b05b
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2026-03-28 11:55:58 +0100

    add translations for Aboutalli

--- src/utils/i18next.js
+++ src/utils/i18next.js
@@ -7,6 +7,8 @@ import contactEN from "../assets/locales/contact/en.json"
 import aboutUsEN from "../assets/locales/aboutUs/en.json"
 import menuDE from "../assets/locales/menu/de.json"
 import menuEN from "../assets/locales/menu/en.json"
+import aboutAlliDE from "../assets/locales/aboutAlli/de.json"
+import aboutAlliEN from "../assets/locales/aboutAlli/en.json"
 
 const i18n = i18next
     .use(initReactI18next)
@@ -18,11 +20,13 @@ const i18n = i18next
                 menu: menuDE,
                 contact: contactDE, // this is the "namespace" so we can have more json files
                 aboutUs: aboutUsDE,
+                aboutAlli: aboutAlliDE,
             },
             en: {
                 menu: menuEN,
                 contact: contactEN,
                 aboutUs: aboutUsEN,
+                aboutAlli: aboutAlliEN,
             },
         },
         fallbackLng: "de",
