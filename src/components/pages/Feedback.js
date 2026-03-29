127a74b9849fe72b6593f07a78cbcd84f3341901
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2026-03-29 19:15:02 +0200

    add translation to feedback page

--- src/components/pages/Feedback.js
+++ src/components/pages/Feedback.js
@@ -7,20 +7,20 @@ const Feedback = () => {
     const { t } = useTranslation("feedback");
     return(
         <div className={styles.aboutAlli}>
-        <Layout route="Feedback">
-            <ContentSection>
-            <h1>{t("title")}</h1>
-                <p>
-                    <Trans i18nKey="content"
-                        components={[<a href="mailto:team@alli-app.com"
-                           aria-label="Send us a mail"
-                           className={styles.mailSection}
-                            key="0"
-                        />]
-                    }>{t("content")}</Trans>
-                </p>
-            </ContentSection>
-        </Layout>
+            <Layout route={t("title")}>
+                <ContentSection>
+                <h1>{t("title")}</h1>
+                    <p>
+                        <Trans i18nKey="content"
+                            components={[<a href="mailto:team@alli-app.com"
+                               aria-label="Send us a mail"
+                               className={styles.mailSection}
+                                key="0"
+                            />]
+                        }>{t("content")}</Trans>
+                    </p>
+                </ContentSection>
+            </Layout>
         </div>
     )
 }
