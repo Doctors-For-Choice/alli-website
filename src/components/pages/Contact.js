dd21daac84dea890274d991b60e48a0f2ffb674a
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2026-03-29 19:13:54 +0200

    fix: format of heading that was too small

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,7 @@ import Layout from "../Layout";
 import styles from "./Contact.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { useTranslation } from "react-i18next";
+import {Trans, useTranslation} from "react-i18next";
 
 const Contact = () => {
   const { t } = useTranslation("contact"); // not passing any namespace will use the defaultNS (by default set to 'translation')
@@ -12,19 +12,23 @@ const Contact = () => {
       <Layout route={t("title")}>
         <ContentSection>
           <h1>{t("title")}</h1>
-          {t("content").split("\n").map(line => <p>{line}</p>)}
-          <a
-            href="mailto:team@alli-app.com"
-            aria-label="Send us a mail"
-            className={styles.mailSection}
-          >
-            <FontAwesomeIcon
-              className={styles.envelopeIcon}
-              icon={faEnvelope}
-              aria-hidden="true"
-            />
-            <div className={styles.link}>team@alli-app.com</div>
-          </a>
+          <p>
+            {t("content")}
+          </p>
+          <p>
+            <a
+              href="mailto:team@alli-app.com"
+              aria-label="Send us a mail"
+              className={styles.mailSection}
+            >
+              <FontAwesomeIcon
+                className={styles.envelopeIcon}
+                icon={faEnvelope}
+                aria-hidden="true"
+              />
+              <div className={styles.link}>team@alli-app.com</div>
+            </a>
+          </p>
         </ContentSection>
       </Layout>
     </div>
